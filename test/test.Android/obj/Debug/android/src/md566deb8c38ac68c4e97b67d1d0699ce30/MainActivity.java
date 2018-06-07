c627dbe5af13b2dcdf622ae9f3250fc61202ed1a
meghanatrsl <meghana.thiruchuri@neudesic.com>
2018-06-07 22:59:13 +0530

    Adding Xamarin forms

---
+++ test/test.Android/obj/Debug/android/src/md566deb8c38ac68c4e97b67d1d0699ce30/MainActivity.java
@@ -0,0 +1,47 @@
+package md566deb8c38ac68c4e97b67d1d0699ce30;
+
+
+public class MainActivity
+	extends md51558244f76c53b6aeda52c8a337f2c37.FormsAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("test.Droid.MainActivity, test.Android", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("test.Droid.MainActivity, test.Android", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
---
+++ test/test/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace test
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new test.Page1());
+        }
+
+        async void Button_Clicked_1(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new test.Page1 ());
+        }
+    }
+}
